e955a5a37d2bfdcc2d9d434950b02fb578748ed1
Jeff <jeffreymanzione@gmail.com>
2021-09-12 21:17:36 -0700

    Parser code compiles now,.

---
+++ lang/parser/parser.h
@@ -0,0 +1,39 @@
+#ifndef LANGUAGE_TOOLS_LANG_PARSER_PARSER_H_
+#define LANGUAGE_TOOLS_LANG_PARSER_PARSER_H_
+
+#include "alloc/arena/arena.h"
+#include "lang/lexer/token.h"
+#include "struct/alist.h"
+#include "struct/q.h"
+
+typedef struct _SyntaxTree SyntaxTree;
+typedef struct _Parser Parser;
+
+typedef SyntaxTree *(*RuleFn)(Parser *parser);
+
+struct _SyntaxTree {
+  bool matched, has_children;
+  Token *token;
+  AList children;
+};
+
+struct _Parser {
+  __Arena st_arena;
+  RuleFn root;
+  Q *tokens;
+};
+
+SyntaxTree NO_MATCH = {.matched = false, .has_children = false};
+SyntaxTree MATCH_EPSILON = {
+    .matched = true, .token = NULL, .has_children = false};
+
+void parser_init(Parser *parser, RuleFn root);
+void parser_parse(Parser *parser, Q *tokens);
+void parser_finalize(Parser *parser);
+Token *parser_next(Parser *parser);
+SyntaxTree *parser_create_st(Parser *parser);
+void parser_delete_st(Parser *parser, SyntaxTree *st);
+void syntax_tree_add_child(SyntaxTree *st, SyntaxTree *child);
+SyntaxTree *match(Parser *parser);
+
+#endif /* LANGUAGE_TOOLS_LANG_PARSER_PARSER_H_ */
--- lang/parser/parser_test.c
+++ lang/parser/parser_test.c
@@ -10,17 +10,12 @@ int main(int argc, const char *args[]) {
   ParserBuilder *pb = parser_builder_create();
 
   parser_builder_rule(pb, "identifier", token(TOKEN_WORD));
-
   parser_builder_rule(pb, "or1",
                       or2(and2(token(KEYWORD_OR), rule("or")), epsilon()));
   parser_builder_rule(pb, "or", and2(rule("identifier"), rule("or1")));
 
   parser_builder_set_root(pb, rule("or"));
-
-  printf("%s", token_type_to_str(1));
-
   parser_builder_print(pb, token_type_to_str, stdout);
-
   parser_builder_delete(pb);
 
   intern_finalize();
